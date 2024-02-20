df49a20416da58b3b2c3354bf855c4ace9441bfb
Areej11002 <160529992+Areej11002@users.noreply.github.com>
2024-02-21 00:26:33 +0500

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,5 @@
+"use strict";
+//2. personal Massage: store a person's name in a varible, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+//would you like to learn some python today?”
+let firstName = "Eric";
+console.log(`hello ${firstName}, “would you like to learn some python today?” `);
